937ced919ccea8367ced3ac5708f3cc77f9ab25c
Lemonsprig <ianselbyuk@gmail.com>
2022-09-26 16:47:36 +0100

    Added buttons to clear the grid and set its size.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 const gamearea = document.querySelector('.game-area');
 const gameareHeight = gamearea.clientHeight;
 const gameareaWidth = gamearea.clientWidth;
+const btnClear = document.querySelector('#clear').addEventListener('click', clearGrid);
+const btnSetSize = document.querySelector('#setSize').addEventListener('click', setSize);
 
 let gridSize = 16;
-columns = `repeat(${gridSize}, 1fr);`
-gamearea.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-gamearea.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+
 
 function colour() {
     this.style.backgroundColor = "red";
@@ -13,14 +14,34 @@ function colour() {
 
 function randomColour(){
     let randomColour = Math.floor(Math.random()*16777215).toString(16);
-    this.style.backgroundColor = "#" + randomColour;
-    
+    this.style.backgroundColor = "#" + randomColour;    
 }
 
-for (i=0; i<(gridSize*gridSize); i++) {
-    const cell = document.createElement('div');
-    cell.classList = "cell";
-    cell.id = `cell-${i}`;
-    gamearea.appendChild(cell);
-    cell.addEventListener('mouseover', randomColour);
+function clearGrid() {
+    const cells = gamearea.querySelectorAll('.cell');
+    //cells.forEach(cell => cell.style.backgroundColor = "#FFFFFF");
+    cells.forEach(cell => cell.remove());
+    drawGrid();
 }
+
+function setSize() {
+    newSize = Number(prompt('Please enter a number between 1 and 100. This will set the size of your grid'));
+    if(newSize != null && typeof(newSize)== "number" && newSize > 0 && newSize <=100) {
+        gridSize = newSize;
+        clearGrid();
+    }
+}
+
+function drawGrid() {
+    gamearea.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gamearea.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    for (i=0; i<(gridSize*gridSize); i++) {
+        const cell = document.createElement('div');
+        cell.classList = "cell";
+        cell.id = `cell-${i}`;
+        gamearea.appendChild(cell);
+        cell.addEventListener('mouseover', randomColour);
+    }
+}
+
+drawGrid();
